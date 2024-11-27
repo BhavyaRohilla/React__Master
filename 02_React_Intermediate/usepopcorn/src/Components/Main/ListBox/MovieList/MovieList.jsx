6920c1a5c75b0689205ae47850449a7c5ae724f3
Bhavya <officialbhavyarohilla@gmail.com>
2024-11-27 23:49:38 +0530

    d8

---
+++ 02_React_Intermediate/usepopcorn/src/Components/Main/ListBox/MovieList/MovieList.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Movie from "./Movie/Movie";
+
+const MovieList = ({ tempMovieData }) => {
+  const [movies, setMovies] = useState(tempMovieData);
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
