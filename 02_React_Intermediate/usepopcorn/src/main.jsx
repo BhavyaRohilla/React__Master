6ad73b8f73a74b62649dde0c3741b07f3ef5c98c
Bhavya <officialbhavyarohilla@gmail.com>
2024-12-08 22:36:11 +0530

    d9

--- 02_React_Intermediate/usepopcorn/src/main.jsx
+++ 02_React_Intermediate/usepopcorn/src/main.jsx
@@ -1,28 +1,30 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
+import Appv1 from "./Appv1";
+import "./index.css";
 // import App from './App.jsx'
 
-import StarRating from "./Components/StarRating";
+// import StarRating from "./Components/StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} Stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} Stars</p>
+//     </div>
+//   );
+// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StarRating
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
     />
     <StarRating maxRating={10} size={24} color="red" className="test" />
-    <Test />
+    <Test /> */}
+    <Appv1 />
   </StrictMode>
 );
